e5ce38888de225097c367fe6d882d359bde46b1f
Alex Ball <aball6@mail.greenriver.edu>
2017-06-17 21:47:02 -0700

    Replaced Sprites with simple rectangles. Decided to go for a game where you break bricks with a ball by bouncing off a paddle.

--- game.js
+++ game.js
@@ -2,8 +2,9 @@ const FRAMES_PER_SECOND = 60;
 const CANVAS_ID = "mainGameCanvas";
 const CANVAS_WIDTH = 680;
 const CANVAS_HEIGHT = 480;
-const TILE_WIDTH = 32;
+const TILE_WIDTH = 64;
 const TILE_HEIGHT = 32;
+const WALL_WIDTH = 20;
 
 const canvas = new Canvas(CANVAS_ID, CANVAS_WIDTH, CANVAS_HEIGHT);
 
@@ -17,12 +18,24 @@ $(document).ready(() => {
     canvas.context.fillStyle = "black";
     canvas.context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    for (let y = 0; y < 10; y++) {
+    //draw bricks
+    for (let y = 1; y < 4; y++) {
       for (let x = 0; x < 10; x++) {
-        let tileX = x * TILE_WIDTH;
+        let tileX = WALL_WIDTH + (x * TILE_WIDTH);
         let tileY = y * TILE_HEIGHT;
-        face.draw(tileX, tileY);
+        canvas.context.beginPath();
+        canvas.context.fillStyle = "black";
+        canvas.context.rect(tileX, tileY, TILE_WIDTH, TILE_HEIGHT);
+        canvas.context.stroke();
+        canvas.context.fillStyle = "yellow";
+        canvas.context.fillRect(tileX, tileY, TILE_WIDTH, TILE_HEIGHT);
       }
     }
+
+    //draw walls
+    canvas.context.fillStyle = "blue";
+    canvas.context.fillRect(0, 0, WALL_WIDTH, CANVAS_HEIGHT);
+    canvas.context.fillRect(CANVAS_WIDTH - WALL_WIDTH, 0, WALL_WIDTH, CANVAS_HEIGHT);
+
   }, 1000 / FRAMES_PER_SECOND);
 });
