3695c941374c3ad20313fc42b4ecc2ec1b48902c
Alex Ball <aball6@mail.greenriver.edu>
2017-06-17 19:41:37 -0700

    Moved canvas styles into javascript so they can be set by constants rather than hardcoded values. Refactored constants such as canvas width and height. Refactored canvas setup into its own function.

--- game.js
+++ game.js
@@ -1,18 +1,37 @@
 const FRAMES_PER_SECOND = 60;
+const CANVAS_ID = "mainGameCanvas";
+const CANVAS_WIDTH = 680;
+const CANVAS_HEIGHT = 480;
 
-const mainGameCanvas = document.getElementById("mainGameCanvas");
-const context = mainGameCanvas.getContext("2d");
+let mainGameCanvas;
+let context;
+
+function prepareCanvas(canvasId, width, height) {
+  mainGameCanvas = document.getElementById(canvasId);
+  context = mainGameCanvas.getContext("2d");
+
+  $(mainGameCanvas).attr({
+    width: width,
+    height: height
+  });
+
+  $(mainGameCanvas).css({
+    width: width + "px",
+    height: height + "px",
+    border: " 1px solid black"
+  });
+}
 
 $(document).ready(() => {
-  const pictureName = "face.png";
-  const face = new Sprite(pictureName);
+  prepareCanvas(CANVAS_ID, CANVAS_WIDTH, CANVAS_HEIGHT);
+  const face = new Sprite("face.png");
 
   let angle = 0;
 
   setInterval(() => {
     //fill background each frame
     context.fillStyle = "black";
-    context.fillRect(0, 0, 800, 800);
+    context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     face.draw(15, 15);
     face.draw(80, 15, 64, 64);
@@ -22,5 +41,4 @@ $(document).ready(() => {
     face.rotate(150, 150, angle);
     angle += 1;
   }, 1000 / FRAMES_PER_SECOND);
-
 });
