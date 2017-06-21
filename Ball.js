af328458a567372dfab41f73f070ab958451067d
Alex Ball <aball6@mail.greenriver.edu>
2017-06-20 21:48:44 -0700

    Added Ball and Paddle classes (code written on my phone, comments are lacking).

---
+++ Ball.js
@@ -0,0 +1,36 @@
+class Ball {
+  constructor(context, x, y, xVelocity, yVelocity, radius) {
+    this.context = context;
+    this.x = x;
+    this.y = y;
+    this.xVelocity = xVelocity;
+    this.yVelocity = yVelocity;
+    this.radius = radius;
+  }
+
+  bounceX() {
+    this.x = -x;
+  }
+
+  bounceY() {
+    this.y = -y;
+  }
+
+  moveX() {
+    this.x += this.xVelocity;
+  }
+
+  moveY() {
+    this.y += this.yVelocity;
+  }
+
+  move() {
+    this.moveX();
+    this.moveY();
+  }
+
+  draw() {
+    //TODO change to draw circle
+    this.context.drawRect(this.x, this.y, this.radius, this.radius);
+  }
+}
---
+++ Paddle.js
@@ -0,0 +1,17 @@
+class Paddle {
+  constructor(context, x, y, width, height) {
+    this.context = context;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  moveLeft(velocity) {
+    this.x -= velocity;
+  }
+
+  moveRight(velocity) {
+    this.y -= velocity;
+  }
+}
